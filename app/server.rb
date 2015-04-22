34406db4268dd29fc11360c45eb10d4951b4d4d9
Joe Newman <jj_newman@hotmail.com>
2015-04-22 23:23:32 +0100

    Update server.rb

--- app/server.rb
+++ app/server.rb
@@ -2,14 +2,11 @@ require 'sinatra'
 require 'data_mapper'
 require 'rack-flash'
 require 'sinatra/partial'
-
 require_relative 'player'
 require_relative 'data_mapper_setup'
 
 enable :sessions
 set :session_secret, 'super secret'
-# use Rack::Flash
-#set :partial_template_engine, :erb
 set :public_folder, Proc.new { File.join(root, '..', "public") }
 set :views, Proc.new { File.join(root, "views") }
 
@@ -19,16 +16,11 @@ end
 
 get '/players/new' do
   erb :"players/new"
-
 end
 
 post '/players' do
  @gkname = params["gk"]
-  # club = params["club"]
-  # cost = params["cost"]
-  # Player.create(:name => name, :club => club, :cost => cost)
   erb :"players/new"
-  # redirect to('/players/final')
 end
 
 get '/players/final' do
